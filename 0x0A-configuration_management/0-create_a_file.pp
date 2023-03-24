bf110ffc4d7e65d70f21c4af1aef2e892c7c4730
Michael Bonyo <bonyomichael406@gmail.com>
2023-03-24 03:00:49 -0700

    correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file school in /tmp directory using puppet
 File { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
