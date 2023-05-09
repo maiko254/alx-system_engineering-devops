4d55338164897873ed89c92767a6952f2f12a9df
maiko254 <bonyomichael406@gmail.com>
2023-05-09 07:40:09 -0700

    querying Reddit API and returning the top 10 hot posts in a given sub

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""queries Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed for a subreddit"""
+
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10".
+                            format(subreddit), headers=headers)
+    if response.status_code == 200:
+        hot_10 = response.json()
+        posts = hot_10['data']['children']
+        for v in posts:
+            print(v['data']['title'])
+    else:
+        return None
