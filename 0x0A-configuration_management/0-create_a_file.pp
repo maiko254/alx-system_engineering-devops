721fd237a99b7a297a3ed2b0f8c4942133958eeb
maiko254 <maiko254@users.noreply.github.com>
2024-01-25 12:21:43 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# creates a file school in tmp directory using puppet
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# kills a process named killmenow
-exec { 'pkill -f killmenow':
-  path   => '/usr/bin:/bin',
-  onlyif => 'pgrep -f killmenow',
-}
