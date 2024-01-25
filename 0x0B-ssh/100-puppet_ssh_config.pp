eeb527563f2cf6b37f52b8f238deed286cef0cb6
maiko254 <maiko254@users.noreply.github.com>
2024-01-25 12:21:53 +0300

    Delete 0x0B-ssh directory

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,8 +0,0 @@
-# using puppet to configure ssh to use the private key ~/.ssh/school and no password authentication
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Host *\n User ubuntu\n IdentityFile ~/.ssh/id_rsa_school\n PasswordAuthentication no\n",
-}
